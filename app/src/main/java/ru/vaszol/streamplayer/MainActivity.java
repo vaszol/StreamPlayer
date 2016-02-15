5597d03d8dd90352e708787dd9ee963834a6f15a
vaszol <vaszol@mail.ru>
2016-02-15 16:49:05 +0300

    Добавляем библиотеку VLC 3.0.0

--- app/src/main/java/ru/vaszol/streamplayer/MainActivity.java
+++ app/src/main/java/ru/vaszol/streamplayer/MainActivity.java
@@ -1,75 +1,38 @@
 package ru.vaszol.streamplayer;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.graphics.PixelFormat;
-import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import org.videolan.libvlc.EventHandler;
-import org.videolan.libvlc.IVideoPlayer;
-import org.videolan.libvlc.LibVLC;
-import org.videolan.libvlc.Media;
-import org.videolan.libvlc.MediaList;
-
-import java.lang.ref.WeakReference;
+//import android.media.MediaPlayer;
+import org.videolan.libvlc.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity
         implements
-        SurfaceHolder.Callback,
-        IVideoPlayer {
-
-    private String urlToStream;
-
-    // Display Surface
-    private LinearLayout vlcContainer;
-    private SurfaceView mSurface;
-    private SurfaceHolder holder;
-
-    // Overlay / Controls
-
-    private FrameLayout vlcOverlay;
-    private ImageView vlcButtonPlayPause;
-    private Handler handlerOverlay;
-    private Runnable runnableOverlay;
-    private Handler handlerSeekbar;
-    private Runnable runnableSeekbar;
-    private SeekBar vlcSeekbar;
-    private TextView vlcDuration;
-    private TextView overlayTitle;
-
-    // media player
-    private LibVLC libvlc;
+        MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnCompletionListener,
+        MediaPlayer.OnPreparedListener, SurfaceHolder.Callback
+{
+    private static final String TAG = "MediaPlayerDemo";
     private int mVideoWidth;
     private int mVideoHeight;
-    private final static int VideoSizeChanged = -1;
+    private MediaPlayer mMediaPlayer;
+    private SurfaceView mPreview;
+    private SurfaceHolder holder;
+    private String path;
+    private Bundle extras;
+    private ProgressBar progressBar;
+    private static final String MEDIA = "media";
+    private static final int LOCAL_AUDIO = 1;
+    private static final int STREAM_AUDIO = 2;
+    private static final int RESOURCES_AUDIO = 3;
+    private static final int LOCAL_VIDEO = 4;
+    private static final int STREAM_VIDEO = 5;
 
-    private MediaPlayer mediaPlayer;
-    private SurfaceHolder vidHolder;
-    private SurfaceView vidSurface;
     String vidAddress = "rtmp://rian.cdnvideo.ru:1935/rr/stream20";
     String vidAddress2 = "sdcard/Download/bootanimation_nexus.mp4";
 
@@ -78,320 +41,124 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         // SETUP THE UI
         setContentView(R.layout.activity_main);
-
-        // Retrieve our url
-//        Bundle b = getIntent().getExtras(); //если берем путь через интент
-//        urlToStream = b.getString("url", null);
-        urlToStream = vidAddress;
-
-        // VLC
-        vlcContainer = (LinearLayout) findViewById(R.id.vlc_container);
-        mSurface = (SurfaceView) findViewById(R.id.vlc_surface);
-
-        // OVERLAY / CONTROLS
-        vlcOverlay = (FrameLayout) findViewById(R.id.vlc_overlay);
-        vlcButtonPlayPause = (ImageView) findViewById(R.id.vlc_button_play_pause);
-        vlcSeekbar = (SeekBar) findViewById(R.id.vlc_seekbar);
-        vlcDuration = (TextView) findViewById(R.id.vlc_duration);
-
-        overlayTitle = (TextView) findViewById(R.id.vlc_overlay_title);
-        overlayTitle.setText(urlToStream);
-
-        // AUTOSTART
-        playMovie();
-    }
-
-    private void showOverlay() {
-        vlcOverlay.setVisibility(View.VISIBLE);
-    }
-
-    private void hideOverlay() {
-        vlcOverlay.setVisibility(View.GONE);
-    }
-
-    private void setupControls() {
-        // PLAY PAUSE
-        vlcButtonPlayPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (libvlc.isPlaying()) {
-                    libvlc.pause();
-                    vlcButtonPlayPause.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_play_over_video));
-                } else {
-                    libvlc.play();
-                    vlcButtonPlayPause.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_pause_over_video));
-                }
-            }
-        });
-
-        // SEEKBAR
-        handlerSeekbar = new Handler();
-        runnableSeekbar = new Runnable() {
-            @Override
-            public void run() {
-                if (libvlc != null) {
-                    long curTime = libvlc.getTime();
-                    long totalTime = (long) (curTime / libvlc.getPosition());
-                    int minutes = (int) (curTime / (60 * 1000));
-                    int seconds = (int) ((curTime / 1000) % 60);
-                    int endMinutes = (int) (totalTime / (60 * 1000));
-                    int endSeconds = (int) ((totalTime / 1000) % 60);
-                    String duration = String.format("%02d:%02d / %02d:%02d", minutes, seconds, endMinutes, endSeconds);
-                    vlcSeekbar.setProgress((int) (libvlc.getPosition() * 100));
-                    vlcDuration.setText(duration);
-                }
-                handlerSeekbar.postDelayed(runnableSeekbar, 1000);
-            }
-        };
-
-        runnableSeekbar.run();
-        vlcSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                Log.v("NEW POS", "pos is : " + i);
-                //if (i != 0)
-                //    libvlc.setPosition(((float) i / 100.0f));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        // OVERLAY
-        handlerOverlay = new Handler();
-        runnableOverlay = new Runnable() {
-            @Override
-            public void run() {
-                vlcOverlay.setVisibility(View.GONE);
-                toggleFullscreen(true);
-            }
-        };
-        final long timeToDisappear = 3000;
-        handlerOverlay.postDelayed(runnableOverlay, timeToDisappear);
-        vlcContainer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                vlcOverlay.setVisibility(View.VISIBLE);
-
-                handlerOverlay.removeCallbacks(runnableOverlay);
-                handlerOverlay.postDelayed(runnableOverlay, timeToDisappear);
-            }
-        });
-    }
-
-    public void playMovie() {
-        if (libvlc != null && libvlc.isPlaying())
-            return;
-        vlcContainer.setVisibility(View.VISIBLE);
-        holder = mSurface.getHolder();
+        extras = getIntent().getExtras();
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        mPreview = (SurfaceView) findViewById(R.id.vlc_surface);
+        holder = mPreview.getHolder();
+        holder.setFixedSize(300, 200);
         holder.addCallback(this);
-        createPlayer(urlToStream);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mMediaPlayer = new MediaPlayer();
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+
     }
 
-
-    private void toggleFullscreen(boolean fullscreen) {
-        WindowManager.LayoutParams attrs = getWindow().getAttributes();
-        if (fullscreen) {
-            attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                vlcContainer.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-            }
-        } else {
-            attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        }
-        getWindow().setAttributes(attrs);
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        setSize(mVideoWidth, mVideoHeight);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //releasePlayer();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        releasePlayer();
-    }
-
-    /**
-     * **********
-     * Surface
-     * ***********
-     */
-
-    public void surfaceCreated(SurfaceHolder holder) {
-    }
-
-    public void surfaceChanged(SurfaceHolder surfaceholder, int format,
-                               int width, int height) {
-        if (libvlc != null)
-            libvlc.attachSurface(surfaceholder.getSurface(), this);
-    }
-
-    public void surfaceDestroyed(SurfaceHolder surfaceholder) {
-    }
-
-    private void setSize(int width, int height) {
-        mVideoWidth = width;
-        mVideoHeight = height;
-        if (mVideoWidth * mVideoHeight <= 1)
-            return;
-
-        // get screen size
-        int w = getWindow().getDecorView().getWidth();
-        int h = getWindow().getDecorView().getHeight();
-
-        // getWindow().getDecorView() doesn't always take orientation into
-        // account, we have to correct the values
-        boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-        if (w > h && isPortrait || w < h && !isPortrait) {
-            int i = w;
-            w = h;
-            h = i;
-        }
-
-        float videoAR = (float) mVideoWidth / (float) mVideoHeight;
-        float screenAR = (float) w / (float) h;
-
-        if (screenAR < videoAR)
-            h = (int) (w / videoAR);
-        else
-            w = (int) (h * videoAR);
-
-        // force surface buffer size
-        if (holder != null)
-            holder.setFixedSize(mVideoWidth, mVideoHeight);
-
-        // set display size
-        ViewGroup.LayoutParams lp = mSurface.getLayoutParams();
-        lp.width = w;
-        lp.height = h;
-        mSurface.setLayoutParams(lp);
-        mSurface.invalidate();
-    }
-
-    @Override
-    public void setSurfaceSize(int width, int height, int visible_width,
-                               int visible_height, int sar_num, int sar_den) {
-        Message msg = Message.obtain(mHandler, VideoSizeChanged, width, height);
-        msg.sendToTarget();
-    }
-
-    /**
-     * **********
-     * Player
-     * ***********
-     */
-
-    private void createPlayer(String media) {
-        releasePlayer();
-        setupControls();
+    private void playVideo(Integer Media) {
         try {
-            if (media.length() > 0) {
-                Toast toast = Toast.makeText(this, media, Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0,
-                        0);
-                toast.show();
-            }
 
-            // Create a new media player
-            libvlc = LibVLC.getInstance();
-            libvlc.setHardwareAcceleration(LibVLC.HW_ACCELERATION_FULL);
-            libvlc.eventVideoPlayerActivityCreated(true);
-            libvlc.setSubtitlesEncoding("");
-            libvlc.setAout(LibVLC.AOUT_OPENSLES);
-            libvlc.setTimeStretching(true);
-            libvlc.setChroma("RV32");
-            libvlc.setVerboseMode(true);
-            LibVLC.restart(this);
-            EventHandler.getInstance().addHandler(mHandler);
-            holder.setFormat(PixelFormat.RGBX_8888);
-            holder.setKeepScreenOn(true);
-            MediaList list = libvlc.getMediaList();
-            list.clear();
-            list.add(new Media(libvlc, LibVLC.PathToURI(media)), false);
-            libvlc.playIndex(0);
+            switch (Media) {
+                case LOCAL_VIDEO:
+                    /*
+                     * TODO: Set the path variable to a local media file path.
+                     */
+                    path = vidAddress2;
+                    if (path == "") {
+                        // Tell the user to provide a media file URL.
+                        Toast
+                                .makeText(
+                                        MainActivity.this,
+                                        "Please edit MediaPlayerDemo_Video Activity, "
+                                                + "and set the path variable to your media file path."
+                                                + " Your media file must be stored on sdcard.",
+                                        Toast.LENGTH_LONG).show();
+
+                    }
+                    break;
+                case STREAM_VIDEO:
+                    /*
+                     * TODO: Set path variable to progressive streamable mp4 or
+                     * 3gpp format URL. Http protocol should be used.
+                     * Mediaplayer can only play "progressive streamable
+                     * contents" which basically means: 1. the movie atom has to
+                     * precede all the media data atoms. 2. The clip has to be
+                     * reasonably interleaved.
+                     *
+                     */
+                    path = vidAddress;
+                    if (path == "") {
+                        // Tell the user to provide a media file URL.
+                        Toast
+                                .makeText(
+                                        MainActivity.this,
+                                        "Please edit MediaPlayerDemo_Video Activity,"
+                                                + " and set the path variable to your media file URL.",
+                                        Toast.LENGTH_LONG).show();
+
+                    }
+
+                    break;
+
+
+            }
+            Toast
+                    .makeText(
+                            MainActivity.this,
+                            path,
+                            Toast.LENGTH_LONG).show();
+
+
+            // Create a new media player and set the listeners
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(path);
+            mMediaPlayer.setDisplay(holder);
+            mMediaPlayer.prepare();
+            mMediaPlayer.setOnBufferingUpdateListener(this);
+            mMediaPlayer.setOnCompletionListener(this);
+            mMediaPlayer.setOnPreparedListener(this);
+//            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+
         } catch (Exception e) {
-            Toast.makeText(this, "Could not create Vlc Player", Toast.LENGTH_LONG).show();
+            Log.e(TAG, "error: " + e.getMessage(), e);
         }
     }
 
-    private void releasePlayer() {
-        if (handlerSeekbar != null && runnableSeekbar != null)
-            handlerSeekbar.removeCallbacks(runnableSeekbar);
-        EventHandler.getInstance().removeHandler(mHandler);
-        if (libvlc == null)
-            return;
-        libvlc.stop();
-        libvlc.detachSurface();
-        holder = null;
-        libvlc.closeAout();
-
-        mVideoWidth = 0;
-        mVideoHeight = 0;
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        Log.d(TAG, "surfaceCreated called");
+//        playVideo(extras.getInt(MEDIA));
+        playVideo(STREAM_VIDEO);
     }
 
-    /**
-     * **********
-     * Events
-     * ***********
-     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.d(TAG, "surfaceChanged called");
+    }
 
-    private Handler mHandler = new MyHandler(this);
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceDestroyed called");
+    }
 
-    private static class MyHandler extends Handler {
-        private WeakReference<MainActivity> mOwner;
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        Log.d(TAG, "onBufferingUpdate percent:" + percent);
+    }
 
-        public MyHandler(MainActivity owner) {
-            mOwner = new WeakReference<MainActivity>(owner);
-        }
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.d(TAG, "onCompletion called");
+    }
 
-        @Override
-        public void handleMessage(Message msg) {
-            MainActivity player = mOwner.get();
-
-            // Player events
-            if (msg.what == VideoSizeChanged) {
-                player.setSize(msg.arg1, msg.arg2);
-                return;
-            }
-
-            // Libvlc events
-            Bundle b = msg.getData();
-            switch (b.getInt("event")) {
-                case EventHandler.MediaPlayerEndReached:
-                    player.releasePlayer();
-                    break;
-                case EventHandler.MediaPlayerPlaying:
-                case EventHandler.MediaPlayerPaused:
-                case EventHandler.MediaPlayerStopped:
-                default:
-                    break;
-            }
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.d(TAG, "onPrepared called");
+        mVideoWidth = mMediaPlayer.getVideoWidth();
+        mVideoHeight = mMediaPlayer.getVideoHeight();
+        if (mVideoWidth != 0 && mVideoHeight != 0) {
+            holder.setFixedSize(mVideoWidth, mVideoHeight);
+            mMediaPlayer.start();
         }
     }
 }
